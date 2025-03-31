9c06aac733a5a2e35a93a1cac88287f7b969e16a
WKeyHeAvEn <kaiimseng@gmail.com>
2025-03-31 11:07:04 +0200

    kästchen 4x4

--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,26 @@
 function setup() {
     createCanvas(400, 400);
     background(220);
+    square(300, 300, 90);
+    square(200, 300, 90);
+    square(100, 300, 90);
+    square(0, 300, 90);
+    square(0, 200, 90);
+    square(0, 100, 90);
+    square(0, 0, 90);
+    square(100, 200, 90);
+    square(100, 100, 90);
+    square(100, 0, 90);
+    square(200, 200, 90);
+    square(200, 100, 90);
+    square(200, 0, 90);
+    square(300, 200, 90);
+    square(300, 100, 90);
+    square(300, 0, 90);
 }
 
 function draw() {
-    ellipse(mouseX, mouseY, 50, 50);
+
+
+
 }
