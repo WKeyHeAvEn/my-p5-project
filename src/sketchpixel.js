c81d59406c8581dd26b39dfc41903a0914d9bc2b
WKeyHeAvEn <kaiimseng@gmail.com>
2025-04-02 11:02:23 +0200

    image hinzufügen und rgb anpassungsfähigf gemacht

--- src/sketchpixel.js
+++ src/sketchpixel.js
@@ -1,10 +1,30 @@
+let img;
+
+function preload() {
+    img = loadImage("../images/image.png");
+}
 
 function setup() {
-    createCanvas(500, 500);
-    img = loadImage("")
+    createCanvas(600, 600);
+    pixelDensity(1);
 }
 
 
 function draw() {
+    background(0);
+    image(img, 50, 50, 600, 600);
+    img.loadPixels();
+    for (let i = 0; i < img.pixels.length; i += 4) {
+        let red = img.pixels[i];
+        let green = img.pixels[i + 1];
+        let blue = img.pixels[i + 2];
+        let alpha = img.pixels[i + 3];
+        img.pixels[i] = red;
+        img.pixels[i + 1] = green;
+        img.pixels[i + 2] = blue;
+        img.pixels[i + 3] = alpha;
 
+
+    }
+    img.updatePixels();
 }
