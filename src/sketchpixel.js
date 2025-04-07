16db9e7bece1e8c0c0a32bbf77fee37c8bb92084
WKeyHeAvEn <kaiimseng@gmail.com>
2025-04-07 08:51:23 +0200

    gitch file hinzugefügt und pixels sorted

--- src/sketchpixel.js
+++ src/sketchpixel.js
@@ -1,32 +1,47 @@
 let img;
+let sortedImg;
 
 function preload() {
-    img = loadImage("../images/image.png");
+    img = loadImage("../images/image.png"); // Bild ins Projektverzeichnis legen
 }
 
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(img.width, img.height);
     pixelDensity(1);
-}
 
-
-function draw() {
-    background(0);
-    image(img, 50, 50, 600, 600);
+    sortedImg = createImage(img.width, img.height);
     img.loadPixels();
-    for (let i = 0; i < img.pixels.length; i += 4) {
-        let red = img.pixels[i];
-        let green = img.pixels[i + 1];
-        let blue = img.pixels[i + 2];
-        let alpha = img.pixels[i + 3];
-        img.pixels[i] = red;
-        img.pixels[i + 1] = green;
-        img.pixels[i + 2] = blue;
-        img.pixels[i + 3] = alpha;
+    sortedImg.loadPixels();
 
+    let pixelsArray = [];
 
+    for (let y = 0; y < img.height; y++) {
+        for (let x = 0; x < img.width; x++) {
+            let index = (x + y * img.width) * 4;
+            let r = img.pixels[index];
+            let g = img.pixels[index + 1];
+            let b = img.pixels[index + 2];
+            let a = img.pixels[index + 3];
 
+            let lightness = (r + g + b) / 3;
 
+            pixelsArray.push({ r, g, b, a, lightness });
+        }
     }
-    img.updatePixels();
+
+    pixelsArray.sort((a, b) => a.lightness - b.lightness); // dunkel nach hell
+
+    for (let i = 0; i < pixelsArray.length; i++) {
+        let x = i % img.width;
+        let y = Math.floor(i / img.width);
+        let index = (x + y * img.width) * 4;
+
+        sortedImg.pixels[index] = pixelsArray[i].r;
+        sortedImg.pixels[index + 1] = pixelsArray[i].g;
+        sortedImg.pixels[index + 2] = pixelsArray[i].b;
+        sortedImg.pixels[index + 3] = pixelsArray[i].a;
+    }
+
+    sortedImg.updatePixels();
+    image(sortedImg, 0, 0);
 }
