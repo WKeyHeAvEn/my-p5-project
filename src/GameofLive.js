b74bf4a775533ff22391ec62f1ca37b6021ffd38
WKeyHeAvEn <kaiimseng@gmail.com>
2025-05-14 10:43:00 +0200

    Update GameofLive.js

--- src/GameofLive.js
+++ src/GameofLive.js
@@ -3,14 +3,14 @@ let grid = [];
 
 function setup() {
     createCanvas(700, 700);
-    cols = width/size;
-    rows = height/size;
+    cols = width / size;
+    rows = height / size;
 
     print(floor(random(10)));
 
-    for (let i=0; i<cols; i++) {
+    for (let i = 0; i < cols; i++) {
         grid[i] = [];
-        for (let j=0; j<rows; j++) {
+        for (let j = 0; j < rows; j++) {
             grid[i][j];
         }
     }
