2e4c6370065dcc6679c2b86b5dbffc4037d98e08
WKeyHeAvEn <kaiimseng@gmail.com>
2025-05-14 10:42:43 +0200

    game of live basics

---
+++ src/GameofLive.js
@@ -0,0 +1,21 @@
+let cols; let rows; let size = 10;
+let grid = [];
+
+function setup() {
+    createCanvas(700, 700);
+    cols = width/size;
+    rows = height/size;
+
+    print(floor(random(10)));
+
+    for (let i=0; i<cols; i++) {
+        grid[i] = [];
+        for (let j=0; j<rows; j++) {
+            grid[i][j];
+        }
+    }
+}
+
+function draw() {
+
+}
